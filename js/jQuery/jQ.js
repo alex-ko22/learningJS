a103e1923edef197d604cd9014bdf80a89bec0b8
Alexander Kozhukhantsev <sash1122@yandex.ru>
2021-04-16 08:09:52 +0300

    Added jQuery/jQ

---
+++ js/jQuery/jQ.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+$( ".button1" ).on( "click", function() {
+   $(window).load(function() {
+    $( alert("Hello, jQuery") ).fadeIn( 3300 )
+   });
+  });
+
---
+++ js/jQuery/jQ.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>jQ</title>
+    <script src="./jquery.js"></script>
+    
+</head>
+<body>
+    <h1> <button class = "button1">Click me!</button></h1>  
+    <script src="./jQ.js"></script>
+</body>
+</html>
